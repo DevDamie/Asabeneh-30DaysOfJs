c1698ad7501a7775d731f5a6ef06f4a6eb6af069
Damilola Adegbola <118520344+DevDamie@users.noreply.github.com>
2024-01-16 17:20:33 +0100

    Update 01-Day.js

--- 30Days-Of-Js/Day1/01-Day.js
+++ 30Days-Of-Js/Day1/01-Day.js
@@ -1,6 +1,47 @@
+// Question 1
 // comments can make code readable
+
+// Question 2
 // Welcome to 30DaysOfJavaScript
 
+//Question 3
 /* comments can make code readable
 easy to reuse and informative */
 
+// Question 6
+let center
+let cloth
+let book
+let pen
+
+// Question 7
+let State = "Kwara" 
+let balls = 23
+let sales = null
+let isGreen = true
+
+// Question 8
+let firstName='Damilola'
+let surName= 'Adegbola'
+let maritalStatus= "married"
+let country= `Nigeria`
+let age= 95
+console.log(firstName)
+console.log(surName)
+console.log(maritalStatus)
+console.log(country)
+console.log(age)
+
+// Question 9
+let firstName= 'Damilola',
+    surName = 'Adegbola',
+    maritalStatus= "married",
+    country= 'Nigeria',
+    age= 95
+console.log(firstName, surName, maritalStatus, country, age)
+
+// Question 10
+let myAge= "I am 25 years old.",
+    yourAge= "You are 30 years old."
+console.log(myAge)
+console.log(yourAge)
